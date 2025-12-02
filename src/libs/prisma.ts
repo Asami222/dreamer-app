efbfb830a9c2dc9deceaf39f258fe8ed8632b32d
Asami222 <asa2_pokecho@icloud.com>
2025-12-02 11:24:34 +0900

    prisma-fix1

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,18 +1,11 @@
-// lib/prisma
-import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pkg from 'pg';
-const { Pool } = pkg;
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: ['query', 'error', 'warn'],
   });
 

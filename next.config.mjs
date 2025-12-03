c3082506fc967b7397fd6cc04be88da3717162f9
Asami222 <asa2_pokecho@icloud.com>
2025-12-03 22:13:42 +0900

    prisma-refix

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+//import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Prismaのバイナリファイル(.so.node)をVercelデプロイ時に確実に含めるための設定
+  // schema.prismaの binaryTargets で生成されたファイルがデプロイパッケージに含まれるようにする
+   outputFileTracingIncludes: {
+       '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+       '/*': ['./node_modules/.prisma/client/**/*'],
+  },
+};
+/*
+const nextConfig: NextConfig = {
+ 
+  reactCompiler: true,
+  typescript: {
+    tsconfigPath: "tsconfig.build.json",
+  },
+};
+*/
+export default nextConfig;
